3020251c05b784f853b31956b1ef408f3ff2a06f
Priscilla <prikoo@gmail.com>
2019-10-05 10:57:23 -0400

    first commit

---
+++ Apomden/Apomden/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apomden
+//
+//  Created by Priscilla Koo on 10/5/19.
+//  Copyright © 2019 Apomden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
