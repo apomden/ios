313a4c5a3d0b5e6a70ee8c7f7f2540b5ba0d891c
Priscilla <prikoo@gmail.com>
2020-03-03 17:13:23 -0500

    Refactoring and Onboarding

---
+++ Apomden/Apomden/LoginFaceIdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Apomden
+//
+//  Created by Priscilla Koo on 3/3/20.
+//  Copyright © 2020 Apomden. All rights reserved.
+//
+
+import UIKit
+
+class LoginFaceIdViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
